4223a9d5f3ad9dcc8fcba83ce592ebe5a4e636e3
Gayrat <gayratbek.sultonov@gmail.com>
2022-02-11 17:14:39 +0500

    Pybabel support languages (en, ru, uz)

--- bot.py
+++ bot.py
@@ -2,34 +2,105 @@ import admin_commands
 import configs
 import handlers
 
-from aiogram import types
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+
 BOT_TOKEN = configs.BOT_TOKEN
-bot = Bot(token=configs.BOT_TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+# Setup bot Dispatcher
+bot = Bot(BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+# Setup i18n middleware
+i18n = configs.Localization(configs.I18N_DOMAIN,
+                            configs.LOCALES_DIR)
+dp.middleware.setup(i18n)
+
+
+# Alias for gettext method
+_ = i18n.lazy_gettext
 
 
 class SetReport(StatesGroup):
     report = State()
 
 
-@dp.message_handler(commands=["start"])
-async def start_menu(message: types.Message):
-    await handlers.start_menu_handler(message)
+START_KEYBOARD = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(_("One"))],
+        [types.KeyboardButton(_("Two"))],
+        [types.KeyboardButton(_("Three"))],
+    ],
+    resize_keyboard=True,
+)
+
+
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    await message.answer(
+        _("Hello, <b>{user}</b>!").format(user=message.from_user.full_name),
+        reply_markup=START_KEYBOARD,
+    )
+
+
+@dp.message_handler(commands="lang")
+async def cmd_lang(message: types.Message, locale):
+    await message.answer(
+        _("Your current language: <i>{language}</i>").format(language=locale)
+    )
+
+
+@dp.message_handler(commands="setlang")
+async def cmd_setlang(message: types.Message):
+    lang = message.get_args()
+
+    if not lang:
+        return await message.answer(_("Specify your language.\nExample: /setlang en"))
+    if lang not in configs.LANGS:
+        return await message.answer(_("This language is not available. Use en or ru"))
+
+    configs.LANG_STORAGE[message.from_user.id] = lang
+
+    await message.answer(_("Language set.", locale=lang))
+
+
+@dp.message_handler(text=_("One"))
+async def text_one(message: types.Message):
+    await message.answer(_("Really one"))
+
+
+@dp.message_handler(text=_("Two"))
+async def text_two(message: types.Message):
+    await message.answer(_("Really two"))
+
+
+@dp.message_handler(text=_("Three"))
+async def text_three(message: types.Message):
+    await message.answer(_("Really three"))
+
+
+@dp.message_handler(commands='lang')
+async def cmd_lang(message: types.Message, locale):
+    # print(locale)
+    # For setting custom lang you have to modify i18n middleware
+    await message.reply(_('Your current language: <i>{language}</i>').format(language=locale))
 
 
 @dp.message_handler(commands=["main"])
 async def menu(message: types.Message):
+    print("LOW", message)
     await handlers.menu_handler(message)
 
 
 @dp.message_handler(commands=['post'])
 async def post(message: types.Message):
+    print("PRONTO", message)
     data = {'type': 'text', 'text': 'text', 'entities': None}
     users = 390736292,
     await admin_commands.send_post_all_users(data, users, bot)
@@ -37,6 +108,7 @@ async def post(message: types.Message):
 
 @dp.message_handler(commands=["report"])
 async def report(message: types.Message):
+    print("XAXA", message)
     await SetReport.report.set()
     await message.answer("Bizga o'z takliflaringizni yuboring!")
 
@@ -44,15 +116,52 @@ async def report(message: types.Message):
 @dp.message_handler(state=SetReport.report,
                     content_types=configs.all_content_types)
 async def report_process(message: types.Message, state: FSMContext):
+    print("PAPA", message)
     await handlers.report_process_handler(message, state, bot)
+
+
+@dp.edited_message_handler()
+async def msg_handler(message: types.Message):
+    print(message)
+
+
+@dp.pre_checkout_query_handler()
+async def pre():
+    print("PRE")
+
+
+@dp.my_chat_member_handler()
+async def some_handler(my_chat_member: types.ChatMemberUpdated):
+    print(my_chat_member)
+
+
+@dp.chat_member_handler()
+async def some_handler(chat_member: types.ChatMemberUpdated):
+    print("BAOBAB", chat_member)
 
 
 @dp.message_handler(content_types=configs.all_content_types)
 async def some_text(message: types.Message):
+    print("DADA", message)
     await handlers.some_text_handler(message, bot)
+
+
+@dp.pre_checkout_query_handler(lambda shipping_query: True)
+async def some_pre_checkout_query_handler(shipping_query: types.ShippingQuery):
+    print("shipping", shipping_query)
+
+
+@dp.shipping_query_handler(lambda shipping_query: True)
+async def some_shipping_query_handler(shipping_query: types.ShippingQuery):
+    print("EEE", shipping_query)
+
+
+@dp.errors_handler()
+async def some_error(baba, error):
+    print("error", baba, error)
 
 
 if __name__ == '__main__':
     executor.start_polling(dp,
                            on_startup=configs.on_startup,
-                           on_shutdown=configs.on_shutdown)
+                           on_shutdown=configs.on_shutdown, skip_updates=True)
--- configs.py
+++ configs.py
@@ -2,26 +2,47 @@ import datetime
 import logging
 import os
 
+from pathlib import Path
+from typing import Tuple, Any
+
+from aiogram import types
+from aiogram.utils.exceptions import BotBlocked, BotKicked, UserDeactivated
+from aiogram.contrib.middlewares.i18n import I18nMiddleware
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-from aiogram.utils.exceptions import BotBlocked, BotKicked, UserDeactivated
-
 load_dotenv()
 
+# Main telegram bot configs
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URL = os.getenv("MONGO_URL")
+
+
+# Telegram chats
 ADMIN_IDS = tuple(os.getenv("ADMIN_IDS").split(","))
 GROUP_ID = int(os.getenv("GROUP_ID"))
 
 
-cluster = MongoClient(MONGO_URL)
-collusers = cluster.bot.users
-collreports = cluster.bot.reports
+# Language
+LANG_STORAGE = {}
+LANGS = ["ru", "en", "uz"]
+I18N_DOMAIN = "mybot"
+BASE_DIR = Path(__file__).parent
+LOCALES_DIR = BASE_DIR / "locales"
 
+
+# Database
+MONGO_URL = os.getenv("MONGO_URL")
+cluster = MongoClient(MONGO_URL)
+collusers = cluster.chatbot.users
+collreports = cluster.chatbot.reports
+
+
+# Telegam supported types
 all_content_types = ["text", "sticker", "photo",
                      "voice", "document", "video", "video_note"]
 
+
+# Logging
 if not os.getenv("DEBUG"):
     formatter = '[%(asctime)s] %(levelname)8s --- %(message)s (%(filename)s:%(lineno)s)'
     logging.basicConfig(
@@ -33,8 +54,29 @@ if not os.getenv("DEBUG"):
     )
 
 
+class Localization(I18nMiddleware):
+    async def get_user_locale(self, action: str, args: Tuple[Any]) -> str:
+        """
+        User locale getter
+        You can override the method if you want to use different way of getting user language.
+        :param action: event name
+        :param args: event arguments
+        :return: locale name
+        """
+        user: types.User = types.User.get_current()
+
+        if LANG_STORAGE.get(user.id) is None:
+            LANG_STORAGE[user.id] = "en"
+        *_, data = args
+        language = data['locale'] = LANG_STORAGE[user.id]
+        return language
+
+
+# On start polling telegram this function running
 async def on_startup(dp):
-    print("Bot are started!")
+    users_lang = collusers.find({}, {"_id": 1, "lang": 1})
+    for i in users_lang:
+        LANG_STORAGE[i.get("_id")] = i.get("lang", "uz")
     for i in ADMIN_IDS:
         try:
             await dp.bot.send_message(i, "Bot are start!")
@@ -42,6 +84,7 @@ async def on_startup(dp):
             pass
 
 
+# On stop polling Telegram, this function running and stopping polling's
 async def on_shutdown(dp):
     logging.warning("Shutting down..")
     for i in ADMIN_IDS:
